b91331e6751bebb3c2d4d0fbc1763a59ba6864fb
Fateh <fatehtariq26@gmail.com>
2023-11-09 13:48:02 +0500

    Added image pose generator for diffused images.

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from imageGenerator import get_images_from_diffusion
+from poses.pose_utils import gen_poses
 
 if __name__ == '__main__':
     imageList = get_images_from_diffusion('A yellow toy lego crane')
+    # Matchers that can be used in 'gen_poses' exhaustive_matcher sequential_matcher
+    gen_poses('/image_list', 'exhaustive_matcher')
