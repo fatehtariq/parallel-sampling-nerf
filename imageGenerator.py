ca201b158821ef00c3d684228d0abfcc6cf9cd3c
Fateh <fatehtariq26@gmail.com>
2023-12-03 14:56:28 +0500

    image list directory fixes.

--- imageGenerator.py
+++ imageGenerator.py
@@ -1,9 +1,8 @@
-import torch
-from diffusers import DDPMParallelScheduler
-from diffusers import StableDiffusionParadigmsPipeline
 import os
-from torchvision.transforms import ToTensor
+import torch
+from torchvision.transforms import ToTensor, functional as F
 from torchvision.utils import save_image
+from diffusers import DDPMParallelScheduler, StableDiffusionParadigmsPipeline
 
 
 def get_images_from_diffusion(prompt):
@@ -18,19 +17,19 @@ def get_images_from_diffusion(prompt):
 
     image_list = pipe(prompt, parallel=ngpu * batch_per_device, num_inference_steps=1000).images
 
-    # Convert PIL images to tensors
-    to_tensor = ToTensor()
-    image_list = [to_tensor(image) for image in image_list]
-
     # Create the output directory if it doesn't exist
-    os.makedirs('scenedir/images', exist_ok=True)
+    output_dir = 'scenedir/images'
+    os.makedirs(output_dir)
 
     # Save the images to the output directory
     for i, image in enumerate(image_list):
-        image_path = os.path.join('scenedir/images', f"image_{i:04d}.png")
-        save_image(image, image_path)
+        # Convert the PIL image to a PyTorch tensor before saving
+        image_tensor = ToTensor()(image)
+        # Save the tensor as an image file
+        image_path = os.path.join(output_dir, f"image_{i:04d}.png")
+        save_image(image_tensor, image_path)
 
     print('[INFO] Number of images created: ', len(image_list))
-    print('[INFO] Saving images...')
+    print('[INFO] Saving images to:', output_dir)
 
     return image_list
